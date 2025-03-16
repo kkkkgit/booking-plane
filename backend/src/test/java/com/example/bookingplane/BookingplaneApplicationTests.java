a535b9f9992a09b53be86b7d7a2ebeaf2435af9e
Kristo Palmik <kristo.palmik@gmail.com>
2025-03-16 10:01:48 +0200

    Initialized backend package with bookingplane instead of demo

---
+++ backend/src/test/java/com/example/bookingplane/BookingplaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingplaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
