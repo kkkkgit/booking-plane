4d82ab280b864df2276a7086d95e72984f712be4
Kristo Palmik <kristo.palmik@gmail.com>
2025-03-17 16:25:01 +0200

    Frontend FlightList page

--- frontend/src/components/FlightList.tsx
+++ frontend/src/components/FlightList.tsx
@@ -4,11 +4,54 @@ import { FlightService } from "../services/FlightService";
 
 const FlightList: React.FC = () => {
     // Component logic
+    const [flights, setFlights] = useState<Flight[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchFlights = async () => {
+            setIsLoading(true);
+            const data = await FlightService.getAllFlights();
+            setFlights(data);
+            setIsLoading(false);
+        };
+
+        fetchFlights();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading flights...</div>
+    }
+
+    if (flights.length === 0) {
+        return <div>There are no flights.</div>
+    }
 
     return (
         // Webpage display logic
         <div className="flight-list">
             <h2>Available Flights</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Destination</th>
+                        <th>Departure</th>
+                        <th>Arrival</th>
+                        <th>Duration</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {flights.map((flight) => (
+                    <tr key={flight.id}>
+                        <td>{flight.destination}</td>
+                        <td>{new Date(flight.departureDate).toLocaleDateString()} {flight.departureTime}</td>
+                        <td>{new Date(flight.arrivalDate).toLocaleDateString()} {flight.arrivalTime}</td>
+                        <td>{flight.flightDuration}</td>
+                        <td>{flight.price}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </div>
     );
 };
